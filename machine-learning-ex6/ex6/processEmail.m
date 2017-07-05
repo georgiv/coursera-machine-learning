d461f36685334690f2bc983469e9b0cd933a190f
georgiv <gd.vassilev@gmail.com>
2017-07-05 21:46:58 +0300

    Exercise SVMs completed

--- machine-learning-ex6/ex6/processEmail.m
+++ machine-learning-ex6/ex6/processEmail.m
@@ -97,17 +97,10 @@ while ~isempty(email_contents)
     %       str2). It will return 1 only if the two strings are equivalent.
     %
 
-
-
-
-
-
-
-
-
+	index = find(strcmp(vocabList, str) == 1);
+	word_indices = [word_indices; index];
 
     % =============================================================
-
 
     % Print to screen, ensuring that the output lines are not too long
     if (l + length(str) + 1) > 78
